b4ef221492ae9bde29d6a0051df6a64bfae842d3
Jazmin <145305037+jaz137@users.noreply.github.com>
2025-04-22 02:29:57 -0400

    sexto commit

--- lib/supabase-singleton.ts
+++ lib/supabase-singleton.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 import type { Database } from "./database.types"
 
-
-const supabaseUrl = "https://ocdxigkvzppqxcgcqajn.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jZHhpZ2t2enBwcXhjZ2NxYWpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNzcyOTEsImV4cCI6MjA2MDg1MzI5MX0.XizXwo8rcsasXJxQ7OJEAqXwnN9XL2tL4GpynNlwvVo"
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -12,8 +10,4 @@ export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: true,
   }
-  
 })
-
-
-
